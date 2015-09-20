dc44df02ccbfa19fb7d0e9dca6937c5f8f492e6c
Vinh <kurei@axcoto.com>
2015-09-20 15:35:48 -0700

    Add hipchat notification

---
+++ example/server_monitoring/hipchat.js
@@ -0,0 +1,28 @@
+//var HipchatBot = require('hipchat-client');
+var HipchatBot = require('hipchatter')
+
+function Bot(option) {
+  this._option = option
+  this.bot    = new HipchatBot(option.token)
+}
+
+// Broadcast a message to all subscribers
+Bot.prototype.yell = function(msg) {
+  console.log('Will notify hipchat ', msg)
+  var token = this._option.token
+  this.bot.notify('System Status',
+                    {
+                      message: msg,
+                      color: 'purple'
+                    }, function(err){
+                      if (err == null) console.log('Successfully notified the room.');
+                    });
+}
+
+Bot.prototype.getName = function() {
+  return 'hipchat'
+}
+
+exports = module.exports = function(option) {
+  return new Bot(option)
+}
